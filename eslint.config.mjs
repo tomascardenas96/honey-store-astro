093dc48c7b80b8c3bceb90e4904d2f70aeb986bf
Tomás Cárdenas <cohesivasalud@gmail.com>
2026-07-04 02:06:53 -0300

    chore: initialization and setup code quality tooling (eslint, prettier, husky, ci)

---
+++ eslint.config.mjs
@@ -0,0 +1,12 @@
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import astro from "eslint-plugin-astro";
+
+export default tseslint.config(
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...astro.configs.recommended,
+  {
+    ignores: ["dist/", ".astro/", "node_modules/"],
+  },
+);
